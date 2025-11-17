916c1b603838fb2e0af3b8e28165a02cb8c627e8
Tankit927 <tonkyponky@proton.me>
2025-11-18 00:31:05 +0530

    Added multiplication exercises

--- ElementaryMath/ElementaryMathPractice.cs
+++ ElementaryMath/ElementaryMathPractice.cs
@@ -82,6 +82,37 @@ class ElementaryMathPractice
     }
 
 
+static (string value, long longValue) GetLongOrExit(string prompt, long? min=null, long? max=null)
+    {
+        // Method to return "Exit" string if ReadLine() returns "Exit"
+        // Or return integer if ReadLine() is integer
+        // Or repeat
+        // min <= integer <= max
+
+        long num;
+        bool isLong;
+        string userInput;
+
+        do
+        {
+            Console.Write(prompt);
+            userInput = Console.ReadLine() ?? "";
+            if (userInput.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return ("exit", 0);
+            }
+
+            isLong = long.TryParse(userInput, out num);
+            if (!isLong || (min != null && num < min) || (max != null && num > max))
+            {
+                Console.WriteLine($"\nEnter a valid integer in range[{(min == null ? int.MinValue : min)},{(max == null ? int.MaxValue : max)}]");
+            }
+        }
+        while (!isLong || (min != null && num < min) || (max != null && num > max));
+
+        return ("", num);
+    }
+
     static void ChooseOperationToPractice()
     {
         // Method to choose certain math operation to practice like:
@@ -91,7 +122,8 @@ class ElementaryMathPractice
         {
             Console.WriteLine("Program to practice elementary math like addition.");
             Console.WriteLine("1. Addition and subtraction");
-            int choice = GetInt("", 1, 1);
+            Console.WriteLine("2. Multiplication");
+            int choice = GetInt("", 1, 2);
 
             Console.Clear();
 
@@ -100,6 +132,11 @@ class ElementaryMathPractice
                 case 1:
                     {
                         PracticeAddition();
+                        break;
+                    }
+                case 2:
+                    {
+                        ChooseMultiplicationType();
                         break;
                     }
                 default:
@@ -112,15 +149,146 @@ class ElementaryMathPractice
     }
 
 
+    static void ChooseMultiplicationType()
+    {
+        // Method to choose type of multiplication to practice
+
+        int previousChoice = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Choose the type of multiplication to practice:");
+            Console.WriteLine("1. Straight line method of multiplication");
+            Console.WriteLine("2. Multiplying numbers close to 10^x");
+            Console.WriteLine("3. Use of additions to multiply");
+            Console.WriteLine("4. Use of percentage to multiply");
+            Console.WriteLine("5. Back to main menu");
+            int choice = GetInt("", 1, 5);
+            if (choice != previousChoice)
+            {
+                previousChoice = choice;
+                ResetScore();
+            }
+
+            Console.Clear();
+
+            switch (choice)
+            {
+                case 1:
+                    {
+                        ChooseNoOfDigitsForStraightLineMethod();
+                        break;
+                    }
+                case 2:
+                    {
+                        ChooseMultiplyingNumbersCloseToCertainPowerOf10();
+                        break;
+                    }
+                case 3:
+                    {
+                        StartTest(12); // Test 12 is multiplying using additions
+                        break;
+                    }
+                case 4:
+                    {
+                        StartTest(13); // Test 13 is multiplying using percentage
+                        break;
+                    }
+                case 5: return;
+                default:
+                    {
+                        Console.WriteLine("Error!");
+                        break;
+                    }
+            }
+        }
+    }
+
+
+    static void ChooseNoOfDigitsForStraightLineMethod()
+    {
+        // Method to choose no. of digits for straight line multiplication method
+
+        int previousChoice = 0;
+        while (true)
+        {
+            Console.WriteLine("Choose no. of digits to practice straight line method of multiplication:");
+            Console.WriteLine("1. 2,3-digit nums");
+            Console.WriteLine("2. 3+ digits");
+            Console.WriteLine("3. Back to multiplication sub-menu");
+            int choice = GetInt("", 1, 3);
+            if (choice != previousChoice)
+            {
+                previousChoice = choice;
+                ResetScore();
+            }
+
+            Console.Clear();
+
+            switch (choice)
+            {
+                case 1:
+                case 2:
+                    {
+                        StartTest(choice+7); // choice starts from 8
+                        break;
+                    }
+                case 3: return;
+                default:
+                    {
+                        Console.WriteLine("Error!");
+                        break;
+                    }
+            }
+        }
+    }
+
+
+    static void ChooseMultiplyingNumbersCloseToCertainPowerOf10()
+    {
+        // Method to choose multiplying numbers close to certain power of 10
+
+        int previousChoice = 0;
+        while (true)
+        {
+            Console.WriteLine("Choose no. of digits to practice straight line method of multiplication:");
+            Console.WriteLine("1. Multiplying numbers close to 100");
+            Console.WriteLine("2. Multiplying numbers close to 1000");
+            Console.WriteLine("3. Back to multiplication sub-menu");
+            int choice = GetInt("", 1, 3);
+            if (choice != previousChoice)
+            {
+                previousChoice = choice;
+                ResetScore();
+            }
+
+            Console.Clear();
+
+            switch (choice)
+            {
+                case 1:
+                case 2:
+                    {
+                        StartTest(choice+9); // choice starts from 10
+                        break;
+                    }
+                case 3: return;
+                default:
+                    {
+                        Console.WriteLine("Error!");
+                        break;
+                    }
+            }
+        }
+    }
+
+
     static void PracticeAddition()
     {
         // Method to choose type of addition practice
-        // 1. Sum two 2-digit +ve integers
-        // 2. Sum ten 2-digit +ve integers
-        // 3. Back to main menu
 
         int previousChoice = 0;
-        while(true)
+        while (true)
         {
             Console.WriteLine("Choose type of addition practice:");
             Console.WriteLine("1. Sum two 2-digit +ve integers.");
@@ -132,7 +300,7 @@ class ElementaryMathPractice
             Console.WriteLine("7. Subtraction of integers in range[10, 99999]");
             Console.WriteLine("8. Back to main menu.");
             int choice = GetInt("", 1, 8);
-            if(choice != previousChoice)
+            if (choice != previousChoice)
             {
                 previousChoice = choice;
                 ResetScore();
@@ -140,7 +308,7 @@ class ElementaryMathPractice
 
             Console.Clear();
 
-            switch(choice)
+            switch (choice)
             {
                 case 1:
                 case 2:
@@ -166,13 +334,13 @@ class ElementaryMathPractice
 
     static void StartTest(int test)
     {
-        // Method to practice addition of 2 digit +ve integers
+        // Method to Start test
 
         TimeSpan time = TimeSpan.FromSeconds(300);
 
         while(true)
         {
-            Console.WriteLine("Sum as many integers as you can before time runs out.");
+            Console.WriteLine("Do the test until time runs out.");
             PrintScoreBoard(time);
             Console.WriteLine();
             Console.WriteLine("1. Start test");
@@ -227,6 +395,36 @@ class ElementaryMathPractice
                                     StartTestSubIntegers10To99999(time);
                                     break;
                                 }
+                            case 8:
+                                {
+                                    StartTest2To3DigitStraightLineMultiplication(time);
+                                    break;
+                                }
+                            case 9:
+                                {
+                                    StartTest3PlusDigitStraightLineMultiplication(time);
+                                    break;
+                                }
+                            case 10:
+                                {
+                                    StartMultiplyingNumbersCloseTo100(time);
+                                    break;
+                                }
+                            case 11:
+                                {
+                                    StartMultiplyingNumbersCloseTo1000(time);
+                                    break;
+                                }
+                            case 12:
+                                {
+                                    StartTestMultiplyingNumbersUsingAdditions(time);
+                                    break;
+                                }
+                            case 13:
+                                {
+                                    StartTestMultiplyingNumbersUsingPercentage(time);
+                                    break;
+                                }
                             default:
                                 {
                                     Console.WriteLine("Error!");
@@ -261,6 +459,294 @@ class ElementaryMathPractice
                     }
             }
         }
+    }
+    
+
+    static void StartTestMultiplyingNumbersUsingAdditions(TimeSpan time)
+    {
+        // Method to practice multiplication of 2,3-digit numbers using additions
+        // or user enters "exit"
+
+        Random rng = new Random();
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        
+        while(stopwatch.Elapsed < time)
+        {
+            Console.WriteLine("Multiply numbers using additions");
+            Console.WriteLine("Enter \"exit\" to go back");
+            PrintScoreBoard(time);
+            Console.WriteLine();
+            
+            int n1 = rng.Next(10, 1000);
+            int n2 = rng.Next(10, 1000);
+            int product = n1 * n2;
+
+            var (stringValue, userInput) = GetIntOrExit($"{n1} x {n2} = ");
+            if (stringValue.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.Clear();
+                break;
+            }
+
+            attemptThisTest += 1;
+            if(product == userInput)
+            {
+                correctThisTest += 1;
+            }
+
+            if(correctThisTest > maxCorrect)
+            {
+                maxCorrect = correctThisTest;
+                maxCorrectCount = attemptThisTest;
+            }
+
+            Console.Clear();
+        }
+
+        stopwatch.Stop();
+        stopwatch.Reset();
+        return;
+    }
+
+
+    static void StartTestMultiplyingNumbersUsingPercentage(TimeSpan time)
+    {
+        // Method to practice multiplication of 2,3-digit numbers using percentage
+        // or user enters "exit"
+
+        Random rng = new Random();
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        
+        while(stopwatch.Elapsed < time)
+        {
+            Console.WriteLine("Multiply numbers using percentage");
+            Console.WriteLine("Enter \"exit\" to go back");
+            PrintScoreBoard(time);
+            Console.WriteLine();
+            
+            int n1 = rng.Next(10, 1000);
+            int n2 = rng.Next(10, 1000);
+            int product = n1 * n2;
+
+            var (stringValue, userInput) = GetIntOrExit($"{n1} x {n2} = ");
+            if (stringValue.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.Clear();
+                break;
+            }
+
+            attemptThisTest += 1;
+            if(product == userInput)
+            {
+                correctThisTest += 1;
+            }
+
+            if(correctThisTest > maxCorrect)
+            {
+                maxCorrect = correctThisTest;
+                maxCorrectCount = attemptThisTest;
+            }
+
+            Console.Clear();
+        }
+
+        stopwatch.Stop();
+        stopwatch.Reset();
+        return;
+    }
+    
+
+    static void StartMultiplyingNumbersCloseTo100(TimeSpan time)
+    {
+        // Method to practice multiplication of numbers close to 100
+        // or user enters "exit"
+
+        Random rng = new Random();
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+
+        while (stopwatch.Elapsed < time)
+        {
+            Console.WriteLine("Multiply numbers close to 100");
+            Console.WriteLine("Enter \"exit\" to go back");
+            PrintScoreBoard(time);
+            Console.WriteLine();
+
+            int n1 = rng.Next(80, 121);
+            int n2 = rng.Next(80, 121);
+            int product = n1 * n2;
+
+            var (stringValue, userInput) = GetIntOrExit($"{n1} x {n2} = ");
+            if (stringValue.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.Clear();
+                break;
+            }
+
+            attemptThisTest += 1;
+            if (product == userInput)
+            {
+                correctThisTest += 1;
+            }
+
+            if (correctThisTest > maxCorrect)
+            {
+                maxCorrect = correctThisTest;
+                maxCorrectCount = attemptThisTest;
+            }
+
+            Console.Clear();
+        }
+
+        stopwatch.Stop();
+        stopwatch.Reset();
+        return;
+    }
+
+
+    static void StartMultiplyingNumbersCloseTo1000(TimeSpan time)
+    {
+        // Method to practice multiplication of numbers close to 1000
+        // or user enters "exit"
+
+        Random rng = new Random();
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        
+        while(stopwatch.Elapsed < time)
+        {
+            Console.WriteLine("Multiply numbers close to 1000");
+            Console.WriteLine("Enter \"exit\" to go back");
+            PrintScoreBoard(time);
+            Console.WriteLine();
+            
+            int n1 = rng.Next(980, 1021);
+            int n2 = rng.Next(980, 1021);
+            int product = n1 * n2;
+
+            var (stringValue, userInput) = GetIntOrExit($"{n1} x {n2} = ");
+            if (stringValue.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.Clear();
+                break;
+            }
+
+            attemptThisTest += 1;
+            if(product == userInput)
+            {
+                correctThisTest += 1;
+            }
+
+            if(correctThisTest > maxCorrect)
+            {
+                maxCorrect = correctThisTest;
+                maxCorrectCount = attemptThisTest;
+            }
+
+            Console.Clear();
+        }
+
+        stopwatch.Stop();
+        stopwatch.Reset();
+        return;
+    }
+
+
+    static void StartTest2To3DigitStraightLineMultiplication(TimeSpan time)
+    {
+        // Method to practice multiplication of 2,3-digit multiplication by straight line method
+        // or user enters "exit"
+
+        Random rng = new Random();
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+
+        while (stopwatch.Elapsed < time)
+        {
+            Console.WriteLine("Multiply numbers using straight line method");
+            Console.WriteLine("Enter \"exit\" to go back");
+            PrintScoreBoard(time);
+            Console.WriteLine();
+
+            int n1 = rng.Next(10, 1000);
+            int n2 = rng.Next(10, 1000);
+            int product = n1 * n2;
+
+            var (stringValue, userInput) = GetIntOrExit($"{n1} x {n2} = ");
+            if (stringValue.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.Clear();
+                break;
+            }
+
+            attemptThisTest += 1;
+            if (product == userInput)
+            {
+                correctThisTest += 1;
+            }
+
+            if (correctThisTest > maxCorrect)
+            {
+                maxCorrect = correctThisTest;
+                maxCorrectCount = attemptThisTest;
+            }
+
+            Console.Clear();
+        }
+
+        stopwatch.Stop();
+        stopwatch.Reset();
+        return;
+    }
+
+
+    static void StartTest3PlusDigitStraightLineMultiplication(TimeSpan time)
+    {
+        // Method to practice multiplication of 2-5 digit multiplication by straight line method
+        // or user enters "exit"
+
+        Random rng = new Random();
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        
+        while(stopwatch.Elapsed < time)
+        {
+            Console.WriteLine("Multiply numbers using straight line method");
+            Console.WriteLine("Enter \"exit\" to go back");
+            PrintScoreBoard(time);
+            Console.WriteLine();
+            
+            int n1 = rng.Next(10, 100000);
+            int n2 = rng.Next(10, 100000);
+            long product = n1 * n2;
+
+            var (stringValue, userInput) = GetLongOrExit($"{n1} x {n2} = ");
+            if (stringValue.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.Clear();
+                break;
+            }
+
+            attemptThisTest += 1;
+            if(product == userInput)
+            {
+                correctThisTest += 1;
+            }
+
+            if(correctThisTest > maxCorrect)
+            {
+                maxCorrect = correctThisTest;
+                maxCorrectCount = attemptThisTest;
+            }
+
+            Console.Clear();
+        }
+
+        stopwatch.Stop();
+        stopwatch.Reset();
+        return;
     }
 
 
